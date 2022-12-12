a0f627f3e8859155b721f953716ec7015d40fc59
kadirakyol <kadir.akyol.ce@gmail.com>
2022-12-12 10:42:09 +0300

    final commit

--- 04.playground/Contents.swift
+++ 04.playground/Contents.swift
@@ -1,3 +1,22 @@
 import UIKit
 
+// Constant - Sabitler
+
+var sayi = 10
+
+sayi = 20
+
+sayi = 40
+
+print(sayi)
+
+let pi = 3.14
+
+// pi = 3 -> hata veriyor. let ileride değiştirilemez.
+
+let klorOrani:Double = 4.5
+
+let isim = "Kadir"
+
+
 
