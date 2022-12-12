2d5d5ee94656506cafd3ff9df28082d61696f686
kadirakyol <kadir.akyol.ce@gmail.com>
2022-12-12 15:21:42 +0300

    final commit

--- 09_Karsilastirma_Oparatörleri.playground/Contents.swift
+++ 09_Karsilastirma_Oparatörleri.playground/Contents.swift
@@ -1,3 +1,23 @@
 import UIKit
 
+var s1 = 60
+var s2 = 50
+
+var y1 = 70
+var y2 = 80
+
+print(s1 == s2)
+print(s1 > s2)
+print(s1 >= s2)
+print(s1 < s2)
+print(s1 <= s2)
+print(s1 != s2) // s1'in içindeki veri s2'nin içindeki veriye eşit değildir. Doğru mu? TRUE
+
+print("\n")
+
+
+print(s1>s2 || y1>y2) // ilk ifade doğru ikinci ifade yanlış. bu veya (or) olduğu için 1v0 = 1 yani true.
+print(s1>s2 && y1>y2) // ilk ifade doğru ikinci ifade yanlış. bu ve (and) işlemi olduğu için 1Λ0 = 0 yani false.
+
+
 
