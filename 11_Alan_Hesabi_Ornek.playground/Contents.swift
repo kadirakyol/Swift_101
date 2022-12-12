2bb384821cc651291128200943a2019b495b0f4c
kadirakyol <kadir.akyol.ce@gmail.com>
2022-12-12 16:56:00 +0300

    final commit

--- 11_Alan_Hesabi_Ornek.playground/Contents.swift
+++ 11_Alan_Hesabi_Ornek.playground/Contents.swift
@@ -1,2 +1,33 @@
 import UIKit
 
+print("Dikdörtgen alanı (1)")
+print("Çember alanı (2)")
+
+print("\n")
+
+var secim = 2
+
+var kisaKenar = 10
+var uzunKenar = 20
+var yariCap = 4
+
+print("Seçiminiz: \(secim) ")
+
+if secim == 1 {
+    print("Dikdörtgen alanı")
+    print("Kısa kenar: \(kisaKenar) ")
+    print("Uzun kenar: \(uzunKenar) ")
+    
+    var alan = kisaKenar * uzunKenar
+    print("Sonuç = \(alan)")
+    
+}
+
+if secim == 2 {
+    print("Çember Alanı")
+    print("Yarı çap: \(yariCap)")
+    var alan = 3.14 * Double(yariCap) * Double(yariCap)
+    print("Sonuç = \(alan)")
+    
+}
+
