ea43211b6fb8c32ab30a4b8e9cbbbaadca9a8f3b
kadirakyol <kadir.akyol.ce@gmail.com>
2022-12-12 16:32:48 +0300

    arr.

--- 10_If_Else.playground/Contents.swift
+++ 10_If_Else.playground/Contents.swift
@@ -1,3 +1,29 @@
 import UIKit
 
+var yas = 17
+var isim = "Kadir"
+
+// Örnek 1
+
+if yas >= 18 {
+    print("Reşitsiniz!")
+}
+
+
+// Örnek 2 (Else)
+
+if yas >= 18 {
+    print("Reşitsiniz")
+} else {
+    print("Reşit Değilsiniz")
+}
+
+// Örnek 3 (String kıyaslama)
+
+if isim == "Kadir" {
+    print("Merhaba Kadir")
+} else {
+    print("Tanınmayan kişi")
+    
+}
 
