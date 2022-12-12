6ea6419da0e938156abc7371bb713f8b9604631c
kadirakyol <kadir.akyol.ce@gmail.com>
2022-12-12 14:56:19 +0300

    arr.

--- 07.playground/Contents.swift
+++ 07.playground/Contents.swift
@@ -1,2 +1,34 @@
 import UIKit
 
+// Sayısaldan sayısala dönüşüm
+
+var i:Int = 42
+var d:Double = 42.45
+var f:Float = 42.89
+
+var sonuc1:Int = Int(d)
+var sonuc2:Double = Double(i)
+var sonuc3:Int = Int(f)
+
+print(sonuc1)
+print(sonuc2)
+print(sonuc3)
+
+print("\n")
+
+// Sayısaldan metine dönüşüm
+
+var str1 = String(i)
+var str2 = String(d)
+var str3 = String(f)
+
+print(str1)
+print(str2)
+print(str3)
+
+print("\n")
+
+// Metinden sayısala dönüşüm
+
+
+
