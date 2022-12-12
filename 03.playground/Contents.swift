6f36890357e82ec16c112707878c65fe6bd2c879
kadirakyol <kadir.akyol.ce@gmail.com>
2022-12-12 10:33:44 +0300

    final commit

--- 03.playground/Contents.swift
+++ 03.playground/Contents.swift
@@ -1,3 +1,30 @@
 import UIKit
 
-// Test
+// Global - Local Değişkenler
+
+class Deneme {
+    
+    var x = 10 //Global
+    var y = 20 //Global
+    
+    func Topla(){
+        
+        var x = 40 //Local
+        x = x + y //x Local (baskın) y global
+        print(x)
+    }
+    
+    func Carp(){
+        
+        x = x * y //ikisi de global
+        print(x)
+    }
+    
+}
+
+var d = Deneme()
+d.Topla()
+d.Carp()
+
+
+
