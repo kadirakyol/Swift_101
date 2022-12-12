2b19bff24ab38dd65e5fb29d20570908c5aa7f91
kadirakyol <kadir.akyol.ce@gmail.com>
2022-12-12 10:48:18 +0300

    final commit

--- 05.playground/Contents.swift
+++ 05.playground/Contents.swift
@@ -2,4 +2,11 @@ import UIKit
 
 // Kaçış karakterleri ve yorum satırları
 
+let yazi = "Kadir \"Naber\" Dedi. \n" // not etc. etc.
+print(yazi)
+
+var yazi1 = "Merhaba bu \"IOS\"\n\teğitiminde \'swift\' dilini öğreneceğiz."
+print(yazi1)
+
+/* Bu şekilde komple blok içine alabiliriz.*/
 
