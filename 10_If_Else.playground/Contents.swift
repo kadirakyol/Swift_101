9e384bbb13800e6edf26deb0dcb03a8593712d00
kadirakyol <kadir.akyol.ce@gmail.com>
2022-12-12 16:41:20 +0300

    final commit

--- 10_If_Else.playground/Contents.swift
+++ 10_If_Else.playground/Contents.swift
@@ -27,3 +27,42 @@ if isim == "Kadir" {
     
 }
 
+// Örnek 4 (Else if)
+
+if isim == "Ahmet" {
+    print("İsminiz Ahmet")
+    }
+else if isim == "Kadir" {
+    print("İsminiz Kadir")
+}
+else {
+    print("İsminizi bilmiyorum")
+}
+
+// Örnek 5 (Çoklu şart)
+
+var kullaniciAdi = "admin"
+var sifre = 1234
+
+if sifre == 1234 && kullaniciAdi == "admin" {
+    print("Doğru Giriş")
+}
+else {
+    print("Hatalı Giriş")
+}
+
+// Örnek 6 (Çoklu şart - OR - )
+
+var sinif = 10
+
+if sinif == 9 || sinif == 10 || sinif == 11 || sinif == 12 {
+    print("Liseli Detected")
+}
+
+// Örnek 7 (Ternary Conditional - 3'lü şart - )
+
+var a = 10
+var b = 20
+
+a == b ? print("a ve b eşit") : print("a ve b eşit değil")
+
