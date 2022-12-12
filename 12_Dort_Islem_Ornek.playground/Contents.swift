18708098c17c4d08b3f5ffb06a6475443176f75d
kadirakyol <kadir.akyol.ce@gmail.com>
2022-12-12 17:04:35 +0300

    final commit

--- 12_Dort_Islem_Ornek.playground/Contents.swift
+++ 12_Dort_Islem_Ornek.playground/Contents.swift
@@ -1,2 +1,30 @@
 import UIKit
 
+print("Toplama (1)")
+print("Çıkarma (2)")
+print("Çarpma (3)")
+print("Bölme (4)")
+
+print("\n")
+
+var tercih = 3
+var sayi1 = 100
+var sayi2 = 20
+
+print("Tercihiniz: \(tercih)")
+
+if tercih == 1 {
+    print("Toplamları = \(sayi1 + sayi2)")
+}
+else if tercih == 2 {
+    print("Farkları = \(sayi1 - sayi2)")
+}
+else if tercih == 3 {
+    print("Çarpımları = \(sayi1 * sayi2)")
+}
+else if tercih == 4 {
+    print("Bölümleri = \(sayi1 / sayi2)")
+}
+else {
+    print("Yanlış bir seçim yaptınız!")
+}
