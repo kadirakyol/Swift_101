cb60e87a4025d9bcf6a1be88267e1fc0cf648737
kadirakyol <kadir.akyol.ce@gmail.com>
2022-12-13 12:48:44 +0300

    final commit

--- 20_Array_Filtreleme.playground/Contents.swift
+++ 20_Array_Filtreleme.playground/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
 
+var sayilar = [1,2,3,4,5,6,7,8,9,10]
 
+var sonuc1 = sayilar.filter ( {$0>7} ) // 7 den büyük dizi üyelerini filtreler
+print(sonuc1)
+
+var sonuc2 = sayilar.filter( {$0<7} ) // 7 den küçük üyeler
+print(sonuc2)
+
+var sonuc3 = sayilar.filter( {$0>3 && $0<7} ) // 3 ten büyük ve 7 den küçük üyeler
+print(sonuc3)
+
+var sonuc4 = sayilar.filter( {$0<4 || $0>5} ) // 4 ten küçük veya 5 ten büyük üyüler
+print(sonuc4)
