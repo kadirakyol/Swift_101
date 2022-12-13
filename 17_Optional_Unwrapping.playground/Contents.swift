8e4d5bf5ccbb05b12c666e433a79ecd40c5f5167
kadirakyol <kadir.akyol.ce@gmail.com>
2022-12-13 11:41:33 +0300

    final commit

--- 17_Optional_Unwrapping.playground/Contents.swift
+++ 17_Optional_Unwrapping.playground/Contents.swift
@@ -1,2 +1,42 @@
 import UIKit
 
+var str:String = "Ahmet"
+
+if str != nil {
+    print(str)
+}
+else {
+    print("str nil değer içeriyor")
+}
+
+print("")
+
+//
+
+var str1:String?
+
+str1 = "Kadir" // str1 i burada tanımladık. ama ünlem ile aşağıda kesinleştirmedik. çıktı optional("kadir") çıkar.
+
+if str1 != nil {
+    print(str1)
+}
+else {
+    print("str1 nil değer içeriyor")
+}
+
+print("")
+
+//
+
+var str2:String?
+
+str2 = "Apo"
+
+if str2 != nil {
+    print(str2!) // ünlem ile kesinleştiriyoruz. Unwrap
+}
+else {
+    print("str2 nil değer içeriyor")
+}
+
+
