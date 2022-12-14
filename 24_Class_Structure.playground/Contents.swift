ee6e0df9fd673044db2d1c8a858f0b38ac46937f
kadirakyol <kadir.akyol.ce@gmail.com>
2022-12-14 13:48:22 +0300

    final commit

--- 24_Class_Structure.playground/Contents.swift
+++ 24_Class_Structure.playground/Contents.swift
@@ -1,3 +1,54 @@
 import UIKit
 
+struct Urun {
+    
+    var ad:String?
+    var fiyat:Double?
+}
 
+class Araba {
+    var renk:String?
+    var kapasite:Int?
+}
+
+var laptop = Urun()
+var bmw = Araba()
+
+laptop.ad = "Macbook"
+laptop.fiyat = 11.999
+
+
+print(laptop.ad!)
+print(laptop.fiyat!)
+
+laptop.fiyat = 10.999
+
+print(laptop.fiyat!)
+
+print("***** \n")
+
+//
+
+bmw.renk = "Kırmızı"
+bmw.kapasite = 4
+
+print(bmw.renk!)
+print(bmw.kapasite!)
+
+if let temp = bmw.renk {
+    print(temp)
+}
+
+var tv = Urun()
+tv.ad = "Samsung"
+tv.fiyat = 4.999
+
+var limuzin = Araba()
+limuzin.renk = "Beyaz"
+limuzin.kapasite = 8
+
+print(tv.fiyat!)
+print(tv.ad!)
+
+print(limuzin.kapasite!)
+print(limuzin.renk!)
