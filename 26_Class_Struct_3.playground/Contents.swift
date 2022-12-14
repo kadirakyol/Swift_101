e79c3d0ae9e66f5469f80d622ad73aff9693c328
kadirakyol <kadir.akyol.ce@gmail.com>
2022-12-14 14:48:05 +0300

    final commit

--- 26_Class_Struct_3.playground/Contents.swift
+++ 26_Class_Struct_3.playground/Contents.swift
@@ -1,2 +1,40 @@
 import UIKit
 
+class Otobus {
+    var kapasite:Int?
+    var nereden:String?
+    var nereye:String?
+    var mevcutYolcu:Int?
+    
+    func yolcuAl(yolcu:Int) {
+        mevcutYolcu! = mevcutYolcu! + yolcu
+    }
+    
+    func yolcuIndir(yolcu:Int) {
+        mevcutYolcu! = mevcutYolcu! - yolcu
+    }
+    
+    func bilgiAl() {
+        print("Kapasite : \(kapasite!)")
+        print("Nereden : \(nereden!)")
+        print("Nereye : \(nereye!)")
+        print("Yolcu Sayısı : \(mevcutYolcu!)")
+    }
+    
+    
+}
+
+
+var kamilKoc = Otobus()
+
+kamilKoc.kapasite = 50
+kamilKoc.nereden = "Bursa"
+kamilKoc.nereye = "Ankara"
+kamilKoc.mevcutYolcu = 10
+
+kamilKoc.bilgiAl()
+
+print("")
+
+kamilKoc.yolcuAl(yolcu: 5)
+kamilKoc.bilgiAl()
